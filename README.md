613b915ba402642b1970294827b665ce6e9d1ca9
Manuel <manuelwiltz@gmail.com>
2023-03-28 12:40:44 +0200

    initial setup

---
+++ README.md
@@ -0,0 +1,8 @@
+## Smart Room
+
+### Project Structure
+#### Frontend
+Angular CLI app
+
+#### Backend
+Java Spring boot Server with MariaDB
---
+++ smartroom_backend/src/main/java/se/smartroom/SmartroomApplication.java
@@ -0,0 +1,13 @@
+package se.smartroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartroomApplication.class, args);
+	}
+
+}
