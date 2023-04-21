b2c743791fab28a767c4f19783253cd37bec64bf
Fuchs Julian <julian.fuchs0701@gmail.com>
2023-04-22 01:02:57 +0200

    Making the frontend buggy while trying to add button functionality

--- smartroom-frontend/src/app/room/rooms/rooms.component.ts
+++ smartroom-frontend/src/app/room/rooms/rooms.component.ts
@@ -1,11 +1,47 @@
-import {Component} from '@angular/core';
+import {Component, AfterViewInit, ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss']
 })
-export class RoomsComponent {
 
+export class RoomsComponent {
+  /*
+   @ViewChild('roomTab', { static: false }) roomTab!: ElementRef;
+
+  loadDetails(roomTab: ElementRef) {
+
+    // Get references to the detailsTab and mainTab elements
+    const editRoom = roomTab.nativeElement.querySelector('.editRoom');
+    const detailsTab = roomTab.nativeElement.querySelector('.detailsTab');
+    const mainTab = roomTab.nativeElement.querySelector('.mainTab');
+   
+    // Show the detailsTab and hide the mainTab
+    editRoom.style.display = 'none';
+    detailsTab.style.display = 'block';
+    mainTab.style.display = 'none';
+
+    
+    console.log("Works loadDetails")
+  }
+
+
+  loadEditData(roomTab: ElementRef) {
+    // Get references to the detailsTab and mainTab elements
+    const editRoom = roomTab.nativeElement.querySelector('.editRoom');
+    const detailsTab = roomTab.nativeElement.querySelector('.detailsTab');
+    const mainTab = roomTab.nativeElement.querySelector('.mainTab');
+
+     
+    // Show the detailsTab and hide the mainTab
+    editRoom.style.display = 'block';
+    detailsTab.style.display = 'none';
+    mainTab.style.display = 'none';
+
+    console.log("Works loadEdit")
+  }*/
 }
+
+
 
