229772af8a39c36a2d1f179cadd73f8c533c8f1c
Manuel <manuelwiltz@gmail.com>
2023-04-25 18:01:16 +0200

    Revert "Making the frontend buggy while trying to add button functionality"
    This reverts commit b2c743791fab28a767c4f19783253cd37bec64bf.

--- smartroom-frontend/src/app/room/rooms/rooms.component.ts
+++ smartroom-frontend/src/app/room/rooms/rooms.component.ts
@@ -1,47 +1,11 @@
-import {Component, AfterViewInit, ViewChild, ElementRef} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss']
 })
-
 export class RoomsComponent {
-  /*
-   @ViewChild('roomTab', { static: false }) roomTab!: ElementRef;
 
-  loadDetails(roomTab: ElementRef) {
-
-    // Get references to the detailsTab and mainTab elements
-    const editRoom = roomTab.nativeElement.querySelector('.editRoom');
-    const detailsTab = roomTab.nativeElement.querySelector('.detailsTab');
-    const mainTab = roomTab.nativeElement.querySelector('.mainTab');
-   
-    // Show the detailsTab and hide the mainTab
-    editRoom.style.display = 'none';
-    detailsTab.style.display = 'block';
-    mainTab.style.display = 'none';
-
-    
-    console.log("Works loadDetails")
-  }
-
-
-  loadEditData(roomTab: ElementRef) {
-    // Get references to the detailsTab and mainTab elements
-    const editRoom = roomTab.nativeElement.querySelector('.editRoom');
-    const detailsTab = roomTab.nativeElement.querySelector('.detailsTab');
-    const mainTab = roomTab.nativeElement.querySelector('.mainTab');
-
-     
-    // Show the detailsTab and hide the mainTab
-    editRoom.style.display = 'block';
-    detailsTab.style.display = 'none';
-    mainTab.style.display = 'none';
-
-    console.log("Works loadEdit")
-  }*/
 }
-
-
 
