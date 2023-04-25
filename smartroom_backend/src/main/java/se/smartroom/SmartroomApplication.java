5c0ae2ff694492bf6667dcbe367a3f23167dcc26
Manuel <manuelwiltz@gmail.com>
2023-04-25 18:01:15 +0200

    Revert "Rooms Section - Frontend"
    This reverts commit a5f1acb1e19ac86914ad8cdaf7ad3157471203da.

--- smartroom_backend/src/main/java/se/smartroom/SmartroomApplication.java
+++ smartroom_backend/src/main/java/se/smartroom/SmartroomApplication.java
@@ -3,7 +3,6 @@ package se.smartroom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class SmartroomApplication {
 
